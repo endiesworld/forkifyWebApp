58a5630b7fa384a3893c75262015336c204115d3
endiesworld <endiesworld@gmail.com>
2019-11-05 05:41:52 +0100

    Second commit from home

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,8 @@
 //https://www.food2fork.com/api/search
 import axios from 'axios';
 async function getResults(params){
-
+    const key = '1532efdf1e45f0102e639656ee0ca9e6' ;
+    const result = await  axios(`https://www.food2fork.com/api/search?key=${key}&q=${params}`) ;
+    console.log(result);
 }
+getResults() ;
